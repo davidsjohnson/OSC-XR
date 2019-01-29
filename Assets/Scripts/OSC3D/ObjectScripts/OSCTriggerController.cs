5971c1fe860c3b26b091ce4ac7dc3be927262d5e
David Johnson <fortjay81@gmail.com>
2019-01-28 20:18:16 -0800

    Fixed OSCTrigger

--- Assets/Scripts/OSC3D/ObjectScripts/OSCTriggerController.cs
+++ Assets/Scripts/OSC3D/ObjectScripts/OSCTriggerController.cs
@@ -1,17 +1,9 @@
 ﻿using UnityEngine;
 using VRTK;
-
 public class OSCTriggerController : OSCTransmitterObjectBase
 {
-
-    [Header("Trigger Pointers")]
-    [Tooltip("Pointer to Trigger the Object (one of right hand or left hand must be populated)")]
-    public VRTK_DestinationMarker leftHandPointer;
-    [Tooltip("Pointer to Trigger the Object (one of right hand or left hand must be populated)")]
-    public VRTK_DestinationMarker rightHandPointer;
-
     public void Start()
-    {
+    { 
         oscAddress = string.IsNullOrEmpty(oscAddress) ? "/trigger" : oscAddress;
     }
 }
--- Assets/Scripts/OSC3D/Utils/OSCTriggerReactor.cs
+++ Assets/Scripts/OSC3D/Utils/OSCTriggerReactor.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using VRTK.Highlighters;
 using VRTK;
 
 public class OSCTriggerReactor : MonoBehaviour
@@ -12,110 +11,25 @@ public class OSCTriggerReactor : MonoBehaviour
     public bool logExitEvent = true;
     public bool logSetEvent = true;
 
-    private VRTK_DestinationMarker leftHandPointer;
-    private VRTK_DestinationMarker rightHandPointer;
 
-    protected virtual void OnEnable()
+    public virtual void DestinationMarkerEnter(object sender, DestinationMarkerEventArgs e)
     {
-        leftHandPointer = oscTrigger.leftHandPointer;
-        rightHandPointer = oscTrigger.rightHandPointer;
-
-        if (leftHandPointer != null && rightHandPointer == null)
-        {
-            VRTK_Logger.Error(VRTK_Logger.GetCommonMessage(VRTK_Logger.CommonMessageKeys.REQUIRED_COMPONENT_MISSING_FROM_GAMEOBJECT, "VRTKExample_PointerObjectHighlighterActivator", "VRTK_DestinationMarker", "the Controller Alias"));
-        }
-
-        if (leftHandPointer != null)
-        {
-            leftHandPointer.DestinationMarkerEnter += DestinationMarkerEnter;
-            leftHandPointer.DestinationMarkerHover += DestinationMarkerHover;
-            leftHandPointer.DestinationMarkerExit += DestinationMarkerExit;
-            leftHandPointer.DestinationMarkerSet += DestinationMarkerSet;
-        }
-
-        if (rightHandPointer != null)
-        {
-            rightHandPointer.DestinationMarkerEnter += DestinationMarkerEnter;
-            rightHandPointer.DestinationMarkerHover += DestinationMarkerHover;
-            rightHandPointer.DestinationMarkerExit += DestinationMarkerExit;
-            rightHandPointer.DestinationMarkerSet += DestinationMarkerSet;
-        }
-
-    }
-
-    protected virtual void OnDisable()
-    {
-        if (leftHandPointer != null)
-        {
-            leftHandPointer.DestinationMarkerEnter -= DestinationMarkerEnter;
-            leftHandPointer.DestinationMarkerHover -= DestinationMarkerHover;
-            leftHandPointer.DestinationMarkerExit -= DestinationMarkerExit;
-            leftHandPointer.DestinationMarkerSet -= DestinationMarkerSet;
-        }
-
-        if (rightHandPointer != null)
-        {
-            rightHandPointer.DestinationMarkerEnter -= DestinationMarkerEnter;
-            rightHandPointer.DestinationMarkerHover -= DestinationMarkerHover;
-            rightHandPointer.DestinationMarkerExit -= DestinationMarkerExit;
-            rightHandPointer.DestinationMarkerSet -= DestinationMarkerSet;
-        }
-    }
-
-    protected virtual void DestinationMarkerEnter(object sender, DestinationMarkerEventArgs e)
-    {
-        ToggleHighlight(e.target, hoverColor);
-        if (logEnterEvent)
-        {
-            DebugLogger(VRTK_ControllerReference.GetRealIndex(e.controllerReference), "POINTER ENTER", e.target, e.raycastHit, e.distance, e.destinationPosition);
-        }
         oscTrigger.SendOSCMessage(string.Format("{0}/enter", oscTrigger.oscAddress));
     }
 
-    private void DestinationMarkerHover(object sender, DestinationMarkerEventArgs e)
+    public  void DestinationMarkerHover(object sender, DestinationMarkerEventArgs e)
     {
-        if (logHoverEvent)
-        {
-            DebugLogger(VRTK_ControllerReference.GetRealIndex(e.controllerReference), "POINTER HOVER", e.target, e.raycastHit, e.distance, e.destinationPosition);
-        }
-        oscTrigger.SendOSCMessage(string.Format("{0}/hover", oscTrigger.oscAddress));
+        //oscTrigger.SendOSCMessage(string.Format("{0}/hover", oscTrigger.oscAddress), oscTrigger.mark.datum["fpath"]);
     }
 
-    protected virtual void DestinationMarkerExit(object sender, DestinationMarkerEventArgs e)
+    public virtual void DestinationMarkerExit(object sender, DestinationMarkerEventArgs e)
     {
-        ToggleHighlight(e.target, Color.clear);
-        if (logExitEvent)
-        {
-            DebugLogger(VRTK_ControllerReference.GetRealIndex(e.controllerReference), "POINTER EXIT", e.target, e.raycastHit, e.distance, e.destinationPosition);
-        }
         oscTrigger.SendOSCMessage(string.Format("{0}/exit", oscTrigger.oscAddress));
     }
 
-    protected virtual void DestinationMarkerSet(object sender, DestinationMarkerEventArgs e)
+    public virtual void DestinationMarkerSet(object sender, DestinationMarkerEventArgs e)
     {
-        ToggleHighlight(e.target, selectColor);
-        if (logSetEvent)
-        {
-            DebugLogger(VRTK_ControllerReference.GetRealIndex(e.controllerReference), "POINTER SET", e.target, e.raycastHit, e.distance, e.destinationPosition);
-        }
         oscTrigger.SendOSCMessage(string.Format("{0}/selected", oscTrigger.oscAddress));
-    }
-
-    protected virtual void ToggleHighlight(Transform target, Color color)
-    {
-        VRTK_BaseHighlighter highligher = (target != null ? target.GetComponentInChildren<VRTK_BaseHighlighter>() : null);
-        if (highligher != null)
-        {
-            highligher.Initialise();
-            if (color != Color.clear)
-            {
-                highligher.Highlight(color);
-            }
-            else
-            {
-                highligher.Unhighlight();
-            }
-        }
     }
 
     protected virtual void DebugLogger(uint index, string action, Transform target, RaycastHit raycastHit, float distance, Vector3 tipPosition)
